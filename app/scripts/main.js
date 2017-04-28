90d11b97315fa23e9afab0e89f01d94b38cbc76e
Henrique Liberato <henriquebmx@gmail.com>
2017-04-28 18:58:21 -0300

    Implementado novas sessões

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
+function scrollTop(element, to, duration) {
+  element = document.body;
+  to = 0;
+  duration = 600;
+  if (duration <= 0) return;
+  var difference = to - element.scrollTop;
+  var perTick = difference / duration * 10;
 
+  setTimeout(function() {
+    element.scrollTop = element.scrollTop + perTick;
+    if (element.scrollTop == to) return;
+    scrollTo(element, to, duration - 10);
+  }, 10);
+};
