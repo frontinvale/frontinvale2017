7927bc9ad623d0d7ca74ea19e621797999540c73
Henrique Liberato <henriquebmx@gmail.com>
2017-05-04 10:52:40 -0300

    Adicionado JS para fechar menu em mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,20 @@ var frontinvale = function() {
   var init = function() {
     removeLoading();
     mapScrollOnClick();
+    closeNavOnClick();
   },
 
   removeLoading = function() {
     document.body.classList.remove('loading');
+  },
+
+  closeNavOnClick = function() {
+    var nav = document.getElementsByTagName('nav')[0];
+    var ul = nav.getElementsByTagName('ul')[0];
+    console.log(ul);
+    ul.addEventListener('click', function (e) {
+      document.getElementById('menu-control').checked = false;
+    });
   },
 
   mapScrollOnClick = function(){
